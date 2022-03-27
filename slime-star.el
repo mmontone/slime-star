08db0317bc4f6fa64462d023faef1180830640d2
Mariano Montone <marianomontone@gmail.com>
2022-03-26 23:03:40 -0300

    SLIME scratch buffer

--- slime-star.el
+++ slime-star.el
@@ -1,6 +1,27 @@
+;;; slime-star --- SLIME with augmented features.  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 Mariano Montone
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;; Code:
+
 (require 'slime)
 
 (defun slime-room ()
+  "Show Common Lisp ROOM information in an Emacs buffer."
   (interactive)
   (let ((room-str
 	 (slime-eval `(cl:with-output-to-string (cl:*standard-output*)
@@ -9,6 +30,14 @@
       (with-current-buffer buffer
 	(insert room-str)
 	(display-buffer buffer)))))
+
+(defun slime-scratch ()
+  "Open the equivalent of an Emacs *scratch* buffer, for Common Lisp/SLIME."
+  (interactive)
+  (let ((buffer (get-buffer-create "*slime-scratch*")))
+    (with-current-buffer buffer
+      (lisp-mode)
+      (switch-to-buffer buffer))))
 
 (defun slime-star-setup-key-bindings ()
   )
