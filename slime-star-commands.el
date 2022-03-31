6e9732685be550322d4344be22c972ad85805013
Mariano Montone <marianomontone@gmail.com>
2022-03-30 22:04:16 -0300

    Put commands in separate file

---
+++ slime-star-commands.el
@@ -0,0 +1,43 @@
+(require 'slime)
+
+(defun slime-room ()
+  "Show Common Lisp ROOM information in an Emacs buffer."
+  (interactive)
+  (let ((room-str
+	 (slime-eval `(cl:with-output-to-string (cl:*standard-output*)
+						(cl:room)))))
+    (let ((buffer (get-buffer-create "*slime-room*")))
+      (with-current-buffer buffer
+	(insert room-str)
+	(display-buffer buffer)))))
+
+(defun slime-scratch ()
+  "Open the equivalent of an Emacs *scratch* buffer, for Common Lisp/SLIME."
+  (interactive)
+  (let ((buffer (get-buffer-create "*slime-scratch*")))
+    (with-current-buffer buffer
+      (lisp-mode)
+      (switch-to-buffer buffer))))
+
+(defun sldb-kill-all-buffers ()
+  "Kill all SLDB buffers."
+  (interactive)
+  (dolist (buf (sldb-buffers))
+    (kill-buffer buf)))
+
+(defun sldb-show-all-frames-details ()
+  "Show details of all frames"
+  (interactive)
+  (let ((inhibit-read-only t)
+        (inhibit-point-motion-hooks t))
+    (sldb-beginning-of-backtrace)
+    ;;(while (get-text-property (point) 'frame)
+    ;;  (sldb-show-frame-details)
+    ;;  (sldb-forward-frame))
+
+    (dotimes (i 5)
+      (when (get-text-property (point) 'frame)
+	(sldb-show-frame-details)
+	(sldb-forward-frame)))))
+
+(provide 'slime-star-commands
--- slime-star.el
+++ slime-star.el
@@ -21,6 +21,7 @@
 (require 'slime)
 (require 'slime-stepper)
 (require 'slime-toolbars)
+(require 'slime-star-commands)
 
 (defgroup slime-star nil
   "SLIME Star (SLIME extensions)."
@@ -35,46 +36,6 @@
   "Use custom toolbars for SLIME buffers."
   :type 'boolean
   :group 'slime-star)
-
-(defun slime-room ()
-  "Show Common Lisp ROOM information in an Emacs buffer."
-  (interactive)
-  (let ((room-str
-	 (slime-eval `(cl:with-output-to-string (cl:*standard-output*)
-						(cl:room)))))
-    (let ((buffer (get-buffer-create "*slime-room*")))
-      (with-current-buffer buffer
-	(insert room-str)
-	(display-buffer buffer)))))
-
-(defun slime-scratch ()
-  "Open the equivalent of an Emacs *scratch* buffer, for Common Lisp/SLIME."
-  (interactive)
-  (let ((buffer (get-buffer-create "*slime-scratch*")))
-    (with-current-buffer buffer
-      (lisp-mode)
-      (switch-to-buffer buffer))))
-
-(defun sldb-kill-all-buffers ()
-  "Kill all SLDB buffers."
-  (interactive)
-  (dolist (buf (sldb-buffers))
-    (kill-buffer buf)))
-
-(defun sldb-show-all-frames-details ()
-  "Show details of all frames"
-  (interactive)
-  (let ((inhibit-read-only t)
-        (inhibit-point-motion-hooks t))
-    (sldb-beginning-of-backtrace)
-    ;;(while (get-text-property (point) 'frame)
-    ;;  (sldb-show-frame-details)
-    ;;  (sldb-forward-frame))
-
-    (dotimes (i 5)
-      (when (get-text-property (point) 'frame)
-	(sldb-show-frame-details)
-	(sldb-forward-frame)))))
 
 (defun slime-star--setup-key-bindings ()
   (define-key sldb-mode-map "Q" 'sldb-kill-all-buffers))
