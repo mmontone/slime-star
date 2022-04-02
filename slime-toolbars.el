463a352e9c60a630dbe576ad3c037f09d4a2eef7
Mariano Montone <marianomontone@gmail.com>
2022-04-02 12:00:39 -0300

    slime-toolbars: fix trace button

--- slime-toolbars.el
+++ slime-toolbars.el
@@ -1,7 +1,7 @@
 ;;; slime-toolbars.el --- Toolbars for SLIME
 
 ;;; Commentary:
-;; 
+;;
 
 (require 'slime)
 
@@ -11,59 +11,59 @@
 
 (defcustom slime-toolbars-slime-tool-bar-spec
   '((slime-compile-defun :help "Compile expression"
-			 :icon "compile")
+                         :icon "compile")
     (slime-eval-last-expression :help "Evaluate expression"
-				:icon "evaluate")
+                                :icon "evaluate")
     (slime-inspect :help "Inspect expression"
-		   :icon "inspect")
+                   :icon "inspect")
     (slime-edit-definition :help "Goto definition"
-			   :icon "jump-to")
+                           :icon "jump-to")
     |
     (slime-break-on-entry :help "Break on entry"
-			  :icon "break")
+                          :icon "break")
     (slime-step-on-entry :help "Step on entry"
-			 :icon "run")
-    (slime-trace :help "Trace function"
-		 :icon "watch")
+                         :icon "run")
+    (slime-toggle-fancy-trace :help "Trace function"
+                              :icon "watch")
     (slime-help-symbol :map slime-help-mode-map
-		       :help "Definition documentation"
-		       :icon "help")
+                       :help "Definition documentation"
+                       :icon "help")
     |
     (lisp-system-browser :help "Open Lisp browser"
-			 :icon "system-browser")
+                         :icon "system-browser")
     (slime-load-file :help "Load Lisp file"
-		     :icon "load-file")
+                     :icon "load-file")
     (slime-compile-file :help "Compile Lisp file"
-			:icon "compile-file")
+                        :icon "compile-file")
     (slime-load-system :help "Load ASDF system"
-		       :icon "load-system"))
+                       :icon "load-system"))
   "SLIME toolbar spec."
   :type '(set symbol)
   :group 'slime-star)
 
 (defcustom slime-toolbars-sldb-tool-bar-spec
   '((sldb-down :help "Go to frame down"
-	       :icon "down")
+               :icon "down")
     (sldb-up :help "Go to frame up"
-	     :icon "up")
+             :icon "up")
     |
     (sldb-toggle-details :help "Toggle frame details"
-			 :icon "all")
+                         :icon "all")
     (sldb-show-source :help "Go to frame source"
-		      :icon "jump-to")
+                      :icon "jump-to")
     (sldb-eval-in-frame :help "Evaluate in frame"
-			:icon "evaluate")
+                        :icon "evaluate")
     (sldb-inspect-in-frame :help "Inspect in frame"
-			   :icon "inspect")
+                           :icon "inspect")
     |
     (sldb-step :help "Step in"
-	       :icon "step")
+               :icon "step")
     (sldb-next :help "Step next"
-	       :icon "next")
+               :icon "next")
     (sldb-out :help "Step out"
-	      :icon "finish")
+              :icon "finish")
     (sldb-quit :help "Quit debugger"
-	       :icon "stop"))
+               :icon "stop"))
   "SLDB toolbar spec."
   :type '(set symbol)
   :group 'slime-star)
@@ -86,28 +86,28 @@ DEFAULTS can contain certain defaults, like :vert-only."
                                   (and map-symbol (eval map-symbol))))
                            default-map))
              (icon (or separator-p
-		       (and (listp item)
+                       (and (listp item)
                             (cl-getf (rest item) :icon))))
-	     (help (and (listp item)
-			(member :help (rest item))
-			(cl-getf (rest item) :help)))
-	     (vert-only (or
-			 (and (listp item)
-			      (member :vert-only (rest item))
-			      (cl-getf (rest item) :vert-only))
-			 (cl-getf defaults :vert-only))))
+             (help (and (listp item)
+                        (member :help (rest item))
+                        (cl-getf (rest item) :help)))
+             (vert-only (or
+                         (and (listp item)
+                              (member :vert-only (rest item))
+                              (cl-getf (rest item) :vert-only))
+                         (cl-getf defaults :vert-only))))
         (if separator-p
             (define-key-after map (make-vector 1 (gensym)) menu-bar-separator)
-	  ;; See 23.18.1.2 Extended Menu Items for the list of available
-	  ;; properties in tool-bar-local-item
-	  ;; See 23.18.6 Tool bars
-	  (if from-map
-	      (tool-bar-local-item-from-menu command icon map from-map
-					     :help help
-					     :vert-only vert-only)
-	    (tool-bar-local-item icon command command map
-				 :vert-only vert-only
-				 :help help)))))
+          ;; See 23.18.1.2 Extended Menu Items for the list of available
+          ;; properties in tool-bar-local-item
+          ;; See 23.18.6 Tool bars
+          (if from-map
+              (tool-bar-local-item-from-menu command icon map from-map
+                                             :help help
+                                             :vert-only vert-only)
+            (tool-bar-local-item icon command command map
+                                 :vert-only vert-only
+                                 :help help)))))
     map))
 
 (defvar slime-tool-bar-map)
