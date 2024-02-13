bd12dd52887e446b2874d6b3e08ed0769bda3b16
Mariano Montone <marianomontone@gmail.com>
2024-02-13 18:18:22 -0300

    slime-trace-buffer

---
+++ slime-trace-buffer.el
@@ -0,0 +1,17 @@
+(require 'slime)
+(require 'cl-lib)
+
+(defun slime-trace-buffer-add-hooks ()
+  (add-hook 'slime-event-hooks 'slime-trace-buffer-event-handler))
+
+(defun slime-trace-buffer-event-handler (event)
+  (slime-dcase event
+    ((:STB/TRACE trace)
+     (let ((buffer (get-buffer-create "*slime-trace*")))
+       (with-current-buffer buffer
+       (insert (prin1-to-string trace))
+       (newline)
+       (unless (get-buffer-window)
+         (display-buffer buffer))))
+     t)
+    (t nil)))
---
+++ swank-trace-buffer.lisp
@@ -0,0 +1,31 @@
+(defpackage swank-trace-buffer
+  (:use :cl))
+
+(in-package :swank-trace-buffer)
+
+;; Code that uses a plain slime stream
+
+(defvar *trace-stream* (swank-buffer-streams:make-buffer-output-stream :trace))
+
+(defun write-trace-to-stream (trace-entry)
+  (princ (swank-trace-dialog::parent-of trace-entry) *trace-stream*)
+  (format *trace-stream* "(~a ~s)"
+          (swank-trace-dialog::spec-of trace-entry)
+          (swank-trace-dialog::args-of trace-entry))
+  (terpri *trace-stream*)
+  (format *trace-stream* "=> ~s" (swank-trace-dialog::retlist-of trace-entry))
+  (terpri *trace-stream*)
+  (finish-output *trace-stream*))
+
+(pushnew 'write-trace-to-stream swank-trace-dialog::*after-trace-hooks*)
+
+;; Code that sends special trace events to Emacs
+(defun send-trace-event (trace)
+  (swank::with-connection (swank::*emacs-connection*)
+    (with-simple-restart
+        (abort "Abort sending trace to Emacs.")
+      (swank::send-to-emacs
+       `(:stb/trace ,(swank-trace-dialog::describe-trace-for-emacs
+                      trace))))))
+
+(pushnew 'send-trace-event swank-trace-dialog::*after-trace-hooks*)
