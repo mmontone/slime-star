e7d5afc3355b371061cfd1be1d1c63ade4955c41
Mariano Montone <marianomontone@gmail.com>
2024-02-13 22:40:32 -0300

    slime-print-buffer

---
+++ slime-print-buffer.el
@@ -0,0 +1,19 @@
+(require 'slime)
+(require 'cl-lib)
+
+(defun slime-print-buffer-add-hooks ()
+  (add-hook 'slime-event-hooks 'slime-print-buffer-event-handler))
+
+(defun slime-print-buffer-event-handler (event)
+  (slime-dcase event
+    ((:spb/print key value)
+     (let ((buffer (get-buffer-create "*slime-print*")))
+       (with-current-buffer buffer
+         (insert (prin1-to-string key))
+         (insert " => ")
+         (insert (prin1-to-string value))
+         (newline)
+         (unless (get-buffer-window)
+           (display-buffer buffer))))
+     t)
+    (t nil)))
