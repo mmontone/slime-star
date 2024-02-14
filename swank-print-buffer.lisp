8fee1dd8e9d35e6a58e4667354a4fbc00aa1640c
Mariano Montone <marianomontone@gmail.com>
2024-02-14 18:54:56 -0300

    print-buffer

--- swank-print-buffer.lisp
+++ swank-print-buffer.lisp
@@ -1,5 +1,6 @@
 (defpackage swank-print-buffer
   (:use :cl)
+  (:nicknames :swankpb)
   (:export #:prn))
 
 (in-package :swank-print-buffer)
@@ -9,30 +10,33 @@
 (defvar *record-printed-p* t)
 
 (defstruct printed
-  key value)
+  expr value)
 
-(defmacro prn (code)
+(defmacro prn (expr)
   (if *record-printed-p*
-      `(let ((printed (make-printed :key ',code :value ,code)))
-         (print-out ',code ,code))
-      `(print-out ',code ,code)))
+      `(let ((printed (make-printed :expr ',expr :value ,expr)))
+         (print-out ',expr ,expr))
+      `(print-out ',expr ,expr)))
+
+(defun print-out (expr value)
+  (send-print-event (make-printed :expr expr :value value)))
 
 ;; Code that uses a plain slime stream
 
 (defvar *print-stream* (swank-buffer-streams:make-buffer-output-stream :print))
 
-(defun print-to-stream (code value)
-  (princ code *print-stream*)
+(defun print-to-stream (expr value)
+  (princ expr *print-stream*)
   (write-string " => " *print-stream*)
   (princ value *print-stream*)
-  (newline)
-  (finish-output *trace-stream*))
+  (terpri *print-stream*)
+  (finish-output *print-stream*))
 
 ;; Code that sends special print events to Emacs
 
 (defun describe-for-emacs (printed)
-  (list (cons 'code (printed-code printed))
-        (cons 'value (printed-value printed))))
+  (list :expr (printed-expr printed)
+        :value (printed-value printed)))
 
 (defun send-print-event (printed)
   (swank::with-connection (swank::*emacs-connection*)
@@ -44,8 +48,10 @@
 (pushnew 'handle-print-event swank::*event-hook*)
 
 (defun handle-print-event (connection event)
+  (declare (ignore connection))
   (swank::dcase event
     ((:spb/print ev)
+     (declare (ignore ev))
      (swank::encode-message event (swank::current-socket-io))
      t)
     (t nil)))
