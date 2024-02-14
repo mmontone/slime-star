0e49fc06577424324cc8c3975a61043a33dc6fbf
Mariano Montone <marianomontone@gmail.com>
2024-02-14 09:53:54 -0300

    trace-buffer

--- swank-trace-buffer.lisp
+++ swank-trace-buffer.lisp
@@ -19,25 +19,55 @@
 
 (pushnew 'write-trace-to-stream swank-trace-dialog::*after-trace-hooks*)
 
+(defun trace-level (trace)
+  (let ((parent (swank-trace-dialog::parent-of trace)))
+    (if (null parent) 0
+        (1+ (trace-level parent)))))
+
+(defun describe-trace-for-emacs (trace)
+  (list :id (swank-trace-dialog::id-of trace)
+        :parent (and (swank-trace-dialog::parent-of trace)
+                     (swank-trace-dialog::id-of (swank-trace-dialog::parent-of trace)))
+        :spec (swank-trace-dialog::spec-of trace)
+        :args (mapcar #'swank::to-line (swank-trace-dialog::args-of trace))
+        :retlist (mapcar #'swank::to-line (swank::ensure-list (swank-trace-dialog::retlist-of trace)))
+        :level (trace-level trace)))
+
 ;; Code that sends special trace events to Emacs
-(defun send-trace-event (trace)
+
+(defun send-trace-enter-event (trace)
   (swank::with-connection (swank::*emacs-connection*)
     (with-simple-restart
         (abort "Abort sending trace to Emacs.")
       (swank::send-to-emacs
-       `(:stb/trace ,(swank-trace-dialog::describe-trace-for-emacs
-                      trace))))))
+       (list :stb/trace-enter (describe-trace-for-emacs trace))))))
 
-(pushnew 'send-trace-event swank-trace-dialog::*after-trace-hooks*)
+(defun send-trace-exit-event (trace)
+  (swank::with-connection (swank::*emacs-connection*)
+    (with-simple-restart
+        (abort "Abort sending trace to Emacs.")
+      (swank::send-to-emacs
+       (list :stb/trace-exit (describe-trace-for-emacs trace))))))
 
+(pushnew 'send-trace-enter-event swank-trace-dialog::*before-trace-hooks*)
+(pushnew 'send-trace-exit-event swank-trace-dialog::*after-trace-hooks*)
+
+;; Handle trace events from SWANK side
 (pushnew 'handle-trace-event swank::*event-hook*)
 
 (defun handle-trace-event (connection event)
   (swank::dcase event
-    ((:stb/trace ev)
+    ((:stb/trace-enter ev)
+     (swank::encode-message event (swank::current-socket-io))
+     t)
+    ((:stb/trace-exit ev)
      (swank::encode-message event (swank::current-socket-io))
      t)
     (t nil)))
 
 (defun inspect-trace (id)
   (swank::inspect-in-emacs (swank-trace-dialog::find-trace id)))
+
+(defun inspect-trace-part (id index)
+  (let ((trace (swank-trace-dialog::find-trace id)))
+    (swank::inspect-in-emacs trace)))
