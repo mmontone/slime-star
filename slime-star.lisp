de45cd58a0e610509c9093742b9c46ec22040e4e
Mariano Montone <marianomontone@gmail.com>
2024-02-17 09:59:47 -0300

    toggle stream output

--- slime-star.lisp
+++ slime-star.lisp
@@ -12,23 +12,29 @@
 
 (in-package :slime-star)
 
-(defmacro def-toggle-stream-to-buffer (name stream-var target-name)
-  `(defun ,name (&optional (enable? 'unset))
-     (let ((enabled? nil)
-           (original-stream ,stream-var))
-       (labels ((enable ()
-                  (setf ,stream-var
-                        (swank-buffer-streams:make-buffer-output-stream ,target-name))
-                  (setf enabled? t))
-                (disable ()
-                  (setf ,stream-var original-stream)
-                  (setf enabled? nil))
-                (toggle ()
-                  (if enabled? (disable) (enable))))
+(defmacro def-toggle-stream-to-buffer (name stream-var stream-value target-name)
+  `(let ((enabled? nil)
+         (original-stream nil))
+     (labels ((enable ()
+                (when (null original-stream)
+                  (setf original-stream ,stream-value))
+                (setf ,stream-var
+                      (uiop:symbol-call 'swank-buffer-streams  'make-buffer-output-stream ,target-name))
+                (setf enabled? t))
+              (disable ()
+                (setf ,stream-var original-stream)
+                (setf enabled? nil))
+              (toggle ()
+                (if enabled? (disable) (enable))))
+       (defun ,name (&optional (enable? 'unset))
          (if (eq enable? 'unset)
              (toggle)
              (if enable? (enable) (disable)))))))
 
-(def-toggle-stream-to-buffer toggle-send-output-to-buffer *standard-output* :standard-output)
-(def-toggle-stream-to-buffer toggle-send-error-to-buffer *error-output* :error-output)
-(def-toggle-stream-to-buffer toggle-send-trace-to-buffer *trace-output* :trace-out)
+(def-toggle-stream-to-buffer toggle-send-output-to-buffer
+  *standard-output* swank::*current-standard-output*
+  :standard-output)
+(def-toggle-stream-to-buffer toggle-send-error-to-buffer
+  *error-output* swank::*current-error-output* :error-output)
+(def-toggle-stream-to-buffer toggle-send-trace-to-buffer
+  *trace-output* swank::*current-trace-output* :trace-out)
