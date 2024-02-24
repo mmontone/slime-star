b435670a3d1edfa9dd9fd4bd6c6407f85570a6f2
Mariano Montone <marianomontone@gmail.com>
2024-02-24 11:08:51 -0300

    Docker install

--- docker/init.el
+++ docker/init.el
@@ -5,10 +5,6 @@
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (package-initialize))
 
-(package-refresh-contents)
-
-(package-install 'slime)
-
 ;; Setup load-path, autoloads and your lisp system
 (add-to-list 'load-path "~/.emacs.d/slime-star")
 
@@ -17,3 +13,6 @@
 (setq inferior-lisp-program "sbcl")
 
 (slime)
+
+(find-file "~/.emacs.d/slime-star/README.org")
+(display-buffer "README.org")
---
+++ docker/install.el
@@ -0,0 +1,8 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-initialize))
+
+(package-refresh-contents)
+
+(package-install 'slime)
