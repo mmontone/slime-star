95995f99e23338910e8408b7d658c4ec69cb7319
Mariano Montone <marianomontone@gmail.com>
2025-01-17 11:07:44 -0300

    Install

---
+++ install.lisp
@@ -0,0 +1,1 @@
+(ql:quickload '(:asdf :alexandria :anaphora :drakma :dexador :fiveam :closer-mop :iterate :do-urlencode :yason :html-entities :slite :montezuma :chipz :archive :string-case :lisp-critic :cl-debug-print :asdf-dependency-graph))
